d4bd7e0be41b3fcf335e5a3fe7662881b53ac369
Zhengang <greedytzg@gmail.com>
2018-11-04 21:01:34 +0800

    日历控件基本架构实现 2018/11/4 21:01 commit

---
+++ app/src/main/java/cn/mt/gd/widget/calendar/CalendarView.java
@@ -0,0 +1,27 @@
+package cn.mt.gd.widget.calendar;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+// TODO: 2018/11/4 对外暴露的属性以及方法的设计，以及接口的设计
+public class CalendarView extends ViewPager {
+
+    /**
+     * 构造方法
+     * @param context
+     */
+    public CalendarView(Context context){
+        this(context,null);
+    }
+
+    /**
+     * 构造方法
+     * @param context
+     * @param attrs
+     */
+    public CalendarView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+
+}
