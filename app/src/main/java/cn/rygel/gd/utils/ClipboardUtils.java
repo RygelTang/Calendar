c8d49e9ad87999641bbc8d15e93e1d914d912b87
Zhengang <greedytzg@gmail.com>
2019-01-09 16:46:52 +0800

    剪贴板工具类修改

--- app/src/main/java/cn/rygel/gd/utils/ClipboardUtils.java
+++ app/src/main/java/cn/rygel/gd/utils/ClipboardUtils.java
@@ -1,32 +1,36 @@
 package cn.rygel.gd.utils;
 
+import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 
 public class ClipboardUtils {
+
     /**
      * 实现文本复制功能
-     * 注意：导包的时候
-     * API 11之前： android.text.ClipboardManager
-     * API 11之后： android.content.ClipboardManager
      *
      * @param context
      * @param content
      */
     public static void copy(Context context, String content) {
-        // 得到剪贴板管理器
-        ClipboardManager cmb = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-        cmb.setText(content.trim());
+        getClipboardManager(context).setText(content.trim());
     }
 
-    /**
-     * 实现粘贴功能
-     *
-     * @return
-     */
-    public static String paste(Context context) {
-        // 得到剪贴板管理器
-        ClipboardManager cmb = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-        return cmb.getText().toString().trim();
+    public static void copy(Context context,String label,String text){
+        ClipData myClip = ClipData.newPlainText(label, text);
+        getClipboardManager(context).setPrimaryClip(myClip);
     }
+
+    public static String paste(Context context) {
+        ClipData clipData = getClipboardManager(context).getPrimaryClip();
+        if(clipData != null && clipData.getItemCount() > 0) {
+            return clipData.getItemAt(0).coerceToText(context).toString();
+        }
+        return null;
+    }
+
+    private static ClipboardManager getClipboardManager(Context context){
+        return (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+    }
+
 }
