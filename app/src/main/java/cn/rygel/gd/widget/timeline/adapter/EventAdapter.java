f51c68a46ff9c575cc9786ba0e2e4867aae2f235
Zhengang <greedytzg@gmail.com>
2019-01-14 19:30:21 +0800

    时间轴事件Adapter

--- app/src/main/java/cn/rygel/gd/widget/timeline/adapter/EventAdapter.java
+++ app/src/main/java/cn/rygel/gd/widget/timeline/adapter/EventAdapter.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import cn.rygel.gd.R;
 import cn.rygel.gd.bean.event.base.BaseEvent;
+import cn.rygel.gd.bean.event.base.DefaultEvent;
+import cn.rygel.gd.bean.event.base.LocationEvent;
 import cn.rygel.gd.bean.event.constants.EventType;
+import cn.rygel.gd.utils.calendar.LunarUtils;
 
 public class EventAdapter extends BaseMultiItemQuickAdapter<BaseEvent,BaseViewHolder> {
 
@@ -22,17 +25,35 @@ public class EventAdapter extends BaseMultiItemQuickAdapter<BaseEvent,BaseViewHo
 
     @Override
     protected void convert(BaseViewHolder helper, BaseEvent item) {
+        helper.setText(R.id.tv_event_name,item.getName());
         switch (helper.getItemViewType()) {
-            case EventType.TYPE_DEFAULT:
-                break;
+            case EventType.TYPE_MEMORIAL:
             case EventType.TYPE_BIRTHDAY:
+                helper.setText(R.id.tv_event_time,formatTime(item.getStart()));
                 break;
             case EventType.TYPE_APPOINTMENT:
-                break;
             case EventType.TYPE_MEETING:
-                break;
-            case EventType.TYPE_MEMORIAL:
+                if(item instanceof LocationEvent) {
+                    helper.setText(R.id.tv_event_location,((LocationEvent) item).getLocation());
+                }
+            case EventType.TYPE_DEFAULT:
+                if(item instanceof DefaultEvent) {
+                    final long duration = ((DefaultEvent) item).getDuration();
+                    final long start = item.getStart();
+                    helper.setText(R.id.tv_event_time,formatTimeString(start, start + duration));
+                }
                 break;
         }
     }
+
+    private static String formatTimeString(long start,long end){
+        return formatTime(start) + " - " + formatTime(end);
+    }
+
+    private static String formatTime(long time){
+        final int hour = (int) (time / 60);
+        final int minute = (int) (time % 60);
+        return (hour < 10 ? "0" + hour : String.valueOf(hour)) + ":" + (minute < 10 ? "0" + minute : String.valueOf(minute));
+    }
+
 }
