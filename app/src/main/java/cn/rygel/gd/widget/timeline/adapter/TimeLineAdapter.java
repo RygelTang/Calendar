8ecf54329646b1f5a889ef7bf341292805e90ea9
Zhengang <greedytzg@gmail.com>
2019-01-30 11:11:49 +0800

    删除事件

--- app/src/main/java/cn/rygel/gd/widget/timeline/adapter/TimeLineAdapter.java
+++ app/src/main/java/cn/rygel/gd/widget/timeline/adapter/TimeLineAdapter.java
@@ -1,7 +1,10 @@
 package cn.rygel.gd.widget.timeline.adapter;
 
 import android.support.annotation.Nullable;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,12 +14,16 @@ import com.chad.library.adapter.base.BaseViewHolder;
 import java.util.List;
 
 import cn.rygel.gd.R;
+import cn.rygel.gd.bean.event.base.BaseEvent;
+import cn.rygel.gd.db.model.EventModel;
 import cn.rygel.gd.ui.event.impl.AddEventActivity;
 import cn.rygel.gd.widget.timeline.bean.TimeLineItem;
 import cn.rygel.gd.utils.calendar.LunarUtils;
 import rygel.cn.uilibrary.utils.UIUtils;
 
 public class TimeLineAdapter extends BaseQuickAdapter<TimeLineItem,BaseViewHolder> {
+
+    EventModel mEventModel = EventModel.getInstance();
 
     public TimeLineAdapter(@Nullable List<TimeLineItem> data) {
         super(R.layout.item_time_line,data);
@@ -35,9 +42,45 @@ public class TimeLineAdapter extends BaseQuickAdapter<TimeLineItem,BaseViewHolde
             }
         });
         if(events.getEvents().size() > 0){
+            EventAdapter adapter = new EventAdapter(events.getEvents());
+            adapter.setOnItemChildClickListener(new OnItemChildClickListener() {
+
+                @Override
+                public void onItemChildClick(final BaseQuickAdapter adapter, View view, final int position) {
+                    PopupMenu popupMenu = new PopupMenu(view.getContext(),view);
+                    MenuInflater inflater = popupMenu.getMenuInflater();
+                    inflater.inflate(R.menu.menu_click_event,popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            BaseEvent event = null;
+                            if(adapter instanceof EventAdapter) {
+                                Object obj =  adapter.getItem(position);
+                                if(obj instanceof BaseEvent) {
+                                    event = (BaseEvent) obj;
+                                } else {
+                                    return false;
+                                }
+                            } else {
+                                return false;
+                            }
+                            switch (menuItem.getItemId()) {
+                                case R.id.action_edit:
+                                    onEditItem(event);
+                                    break;
+                                case R.id.action_delete:
+                                    onDeleteItem(event);
+                                    break;
+                            }
+                            return false;
+                        }
+                    });
+                    popupMenu.show();
+                }
+            });
             btnEmpty.setVisibility(View.GONE);
             rvEvents.setVisibility(View.VISIBLE);
-            rvEvents.setAdapter(new EventAdapter(events.getEvents()));
+            rvEvents.setAdapter(adapter);
         } else {
             btnEmpty.setVisibility(View.VISIBLE);
             rvEvents.setVisibility(View.GONE);
@@ -48,6 +91,14 @@ public class TimeLineAdapter extends BaseQuickAdapter<TimeLineItem,BaseViewHolde
                         events.getDate().solarDay
                 )
         );
+    }
+
+    private void onEditItem(BaseEvent event) {
+
+    }
+
+    private void onDeleteItem(BaseEvent event) {
+        mEventModel.delete(event);
     }
 
     private void createNewEventIn(LunarUtils.Solar solar){
