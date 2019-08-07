b7901c13b2e512c8061ee6d07825e2504acf44a2
Rygel <rygel.tang@gmail.com>
2019-08-07 15:45:31 +0800

    添加Q群跳转

--- app/src/main/java/cn/rygel/gd/ui/about/AboutActivity.java
+++ app/src/main/java/cn/rygel/gd/ui/about/AboutActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +20,7 @@ import skin.support.content.res.SkinCompatUserThemeManager;
 
 public class AboutActivity extends BaseActivity {
 
+    private static final String GROUP_KEY = "JMCMAYQbWXA_cVmnpIHScYKbxhMCm-Yi";
     private static final String EMAIL = "rygel.tang@gmail.com";
     private static final String QQ = "1066340496";
 
@@ -54,6 +57,13 @@ public class AboutActivity extends BaseActivity {
         ClipboardUtils.copy(this,"qq",QQ);
     }
 
+    @OnClick(R.id.layout_qq_group)
+    protected void onAddGroup() {
+        if (!joinQQGroup(this)) {
+            Toast.makeText(this, R.string.not_found_qq, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void loadVersionInfo() {
         PackageManager pm = getPackageManager();
         try {
@@ -67,6 +77,26 @@ public class AboutActivity extends BaseActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_about;
+    }
+
+    /****************
+     *
+     * 发起添加群流程。群号：日历用户群(228434421)
+     * 调用 joinQQGroup(JMCMAYQbWXA_cVmnpIHScYKbxhMCm-Yi) 即可发起手Q客户端申请加群 日历用户群(228434421)
+     *
+     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
+     ******************/
+    private static boolean joinQQGroup(Context context) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + GROUP_KEY));
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            // 未安装手Q或安装的版本不支持
+            return false;
+        }
     }
 
     public static void start(Context context){
